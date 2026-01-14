5191a25871c5202a4ad0f730e638e13186697e80
birciceke <birciceke@gmail.com>
2026-01-14 17:09:26 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
-const BACKEND_URL = process.env.BACKEND_URL || "https://the-quinfall-server.onrender.com";
-
 const nextConfig: NextConfig = {
   devIndicators: false,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${BACKEND_URL}/api/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
